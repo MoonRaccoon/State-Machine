8a4330b54fc9f0274155bc2fb02fcdcbae55ee8e
ShamounSyed <gamemaster1111001@gmail.com>
2014-11-14 09:47:53 -0800

    Initial Commit

---
+++ Code/Scripts/GameData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData : MonoBehaviour 
+{
+	public Texture2D beginStateSplash;
+	public Texture2D lostStateSplash;
+	public Texture2D wonStateSplash;
+
+	[HideInInspector]
+	public int playerLives;
+	
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+	}
+}
